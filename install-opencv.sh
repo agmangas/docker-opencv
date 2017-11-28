7f7d813e4f91d53d79f9389c09e90ee684272125
Andres Garcia Mangas <agmangas@gmail.com>
2017-11-28 18:00:46 +0100

    Remove some unnecessary dependencies and remove sources after install

--- install-opencv.sh
+++ install-opencv.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# download opencv-2.4.13.4
+# Download opencv-2.4.13.4
 
 wget https://github.com/opencv/opencv/archive/2.4.13.4.zip -O opencv-2.4.13.4.zip
 unzip opencv-2.4.13.4.zip
@@ -8,17 +8,43 @@ cd opencv-2.4.13.4
 mkdir release
 cd release
 
-# compile and install
+# Compile and install
 
-cmake -G "Unix Makefiles" -DCMAKE_CXX_COMPILER=/usr/bin/g++ CMAKE_C_COMPILER=/usr/bin/gcc -DCMAKE_BUILD_TYPE=RELEASE -DCMAKE_INSTALL_PREFIX=/usr/local -DWITH_TBB=ON -DBUILD_NEW_PYTHON_SUPPORT=ON -DWITH_V4L=ON -DINSTALL_C_EXAMPLES=ON -DINSTALL_PYTHON_EXAMPLES=ON -DBUILD_EXAMPLES=ON -DWITH_QT=ON -DWITH_OPENGL=ON -DBUILD_FAT_JAVA_LIB=ON -DINSTALL_TO_MANGLED_PATHS=ON -DINSTALL_CREATE_DISTRIB=ON -DINSTALL_TESTS=ON -DENABLE_FAST_MATH=ON -DWITH_IMAGEIO=ON -DBUILD_SHARED_LIBS=OFF -DWITH_GSTREAMER=ON ..
+cmake -G "Unix Makefiles" \
+    -DCMAKE_CXX_COMPILER=/usr/bin/g++ \
+    -DCMAKE_C_COMPILER=/usr/bin/gcc \
+    -DCMAKE_BUILD_TYPE=RELEASE \
+    -DCMAKE_INSTALL_PREFIX=/usr/local \
+    -DWITH_TBB=ON \
+    -DBUILD_NEW_PYTHON_SUPPORT=ON \
+    -DWITH_V4L=ON \
+    -DINSTALL_C_EXAMPLES=ON \
+    -DINSTALL_PYTHON_EXAMPLES=ON \
+    -DBUILD_EXAMPLES=ON \
+    -DWITH_OPENGL=ON \
+    -DBUILD_FAT_JAVA_LIB=ON \
+    -DINSTALL_TO_MANGLED_PATHS=ON \
+    -DINSTALL_CREATE_DISTRIB=ON \
+    -DINSTALL_TESTS=ON \
+    -DENABLE_FAST_MATH=ON \
+    -DWITH_IMAGEIO=ON \
+    -DBUILD_SHARED_LIBS=ON \
+    -DWITH_GSTREAMER=ON \
+    ..
+
 make all
 make install
 
-# ignore libdc1394 error http://stackoverflow.com/questions/12689304/ctypes-error-libdc1394-error-failed-to-initialize-libdc1394
+# Put cv2.so into the PYTHONPATH
 
-apt-get install python-opencv
+ln -s /usr/local/python/2.7/cv2.so /usr/local/lib/python2.7/dist-packages/
 
-# Echoes OpenCV installed version (will print error message if OpenCV2 wasn't installed correctly)
+# Echo OpenCV version
 
-echo -e "OpenCV version:"
-pkg-config --modversion opencv
+echo "OpenCV version:" $(python -c "import cv2; print(cv2.__version__);")
+
+# Remove OpenCV sources folder
+
+cd ../..
+rm -fr opencv-2.4.13.4
+rm opencv-2.4.13.4.zip
